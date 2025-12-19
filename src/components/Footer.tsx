3bb8a50861660a0249159156ac91aeee33c8ea03
midnite-ops <charlesoyims@gmail.com>
2025-12-19 14:19:00 +0100

    Completed the landing page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,69 +5,91 @@ import twitter from '../assets/twitter-icon.svg';
 import phoneIcon from '../assets/phone-icon.svg';
 import locationIcon from '../assets/location-icon.svg';
 import mailIcon from '../assets/mail-icon.svg';
+import CTAbg from '../assets/portfolio/CTA-bg.svg';
 export default function Footer() {
   return (
-    <footer className="py-[50px] xl:px-[100px] bg-primaryBlack text-gray">
-        <div className="container mx-auto ">
-            <div className="flex justify-between items-start border-b border-gray pb-14 mb-10 ">
-                <div className="text-gray w-[25%]">
-                    <img src={explorewhite} alt="Explore Printing Logo" className='mb-6'/>
-                    <p className="text-sm mt-2">
-                        Your trusted partner for all printing needs. Quality, speed, and reliability in every project.
-                    </p>
-                    <div className='flex gap-5 mt-5'>
-                        <a href="">
-                            <img src={facebook} alt="a facebook link" />
-                        </a>
-                        <a href="">
-                            <img src={twitter} alt="a twitter link" />
-                        </a>
-                        <a href="">
-                            <img src={instagram} alt="an instagram logo embedded with a link" />
-                        </a>
-                    </div>
+    <section>
+        <div className='relative h-[500px] flex justify-center items-center text-white'>
+            <img src={CTAbg} alt="a printing press background" className="absolute top-0 left-0 w-full h-full object-cover -z-10"/>
+            <div className='w-[70%] flex justify-center flex-col items-center gap-8 text-center'>
+                <h1 className='font-poppins text-4xl xl:text-5xl font-medium'>Ready to Bring Your Ideas to Life?</h1>
+                <p>
+                    Let's Print! Join thousands of satisfied customers who trust PrintCraft for all their printing needs. Get your custom quote today and see why we're the preferred choice for quality printing.
+                </p>
+                <div>
+                    <button className="btn bg-primary text-white font-poppins text-[18px] px-6 py-2 rounded-md mr-8 hover:bg-primary-500 transition duration-300">
+                        Request a Quote
+                    </button>
+                    <button className="btn bg-transparent text-white font-poppins text-[18px] px-6 py-2 rounded-md border border-white hover:bg-gray-800 transition duration-300">
+                        Call us now
+                    </button>
                 </div>
-                <div className=''>
-                    <h2 className='text-white text-xl mb-6'>Services</h2>
-                    <ul className='flex flex-col gap-5 '>
-                        <li>Merchandise Printing</li>
-                        <li>Billboard & Large Format</li>
-                        <li>Books and Publications</li>
-                        <li>Marketing Materials</li>
-                        <li>Custom Solutions</li>
-                    </ul>
-                </div>
-                <div className=''>
-                    <h2 className='text-white text-xl mb-6'>Quick Links</h2>
-                    <ul className='text-gray flex flex-col gap-5 font-mont'>
-                        <a href="#" className="hover:text-gray-300">About Us</a>
-                        <a href="#" className="hover:text-gray-300">Portfolio</a>
-                        <a href="#" className="hover:text-gray-300">FAQ</a>
-                        <a href="#" className="hover:text-gray-300">Contact Us</a>
-                    </ul>
-                </div>
-                <div className='w-[260px]'>
-                    <h2 className='text-xl mb-6 text-white'>Contact Info</h2>
-                    <ul className='flex flex-col gap-5 font-mont'>
-                        <div className='flex items-center gap-4'>
-                            <img src={locationIcon} alt="a location icon" />
-                            <p className='leading-7'>123, George Street Shomulu, Lagos</p>
-                        </div>
-                        <div className='flex items-center gap-4'>
-                            <img src={phoneIcon} alt="a phone icon" />
-                            <p className='leading-7'>+234 123 456 78</p>
-                        </div>
-                        <div className='flex items-center gap-4'>
-                            <img src={mailIcon} alt="a mail icon" />
-                            <p className='leading-7'>info@xploreprinting.com</p>
-                        </div>
-                    </ul>
-                </div>
-            </div>
-            <div className="mt-8 text-center text-[18px] font-light">
-                  @ {new Date().getFullYear()} Explore-Printing. All rights reserved. | Privacy Policy | Terms of Service
+                
             </div>
         </div>
-    </footer>
+        <footer className="py-[50px] xl:px-[100px] bg-primaryBlack text-gray">
+            <div className="container mx-auto ">
+                <div className="flex justify-between items-start border-b border-gray pb-14 mb-10 ">
+                    <div className="text-gray w-[25%]">
+                        <img src={explorewhite} alt="Explore Printing Logo" className='mb-6'/>
+                        <p className="text-sm mt-2">
+                            Your trusted partner for all printing needs. Quality, speed, and reliability in every project.
+                        </p>
+                        <div className='flex gap-5 mt-5'>
+                            <a href="">
+                                <img src={facebook} alt="a facebook link" />
+                            </a>
+                            <a href="">
+                                <img src={twitter} alt="a twitter link" />
+                            </a>
+                            <a href="">
+                                <img src={instagram} alt="an instagram logo embedded with a link" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <h2 className='text-white text-xl mb-6'>Services</h2>
+                        <ul className='flex flex-col gap-5 '>
+                            <li>Merchandise Printing</li>
+                            <li>Billboard & Large Format</li>
+                            <li>Books and Publications</li>
+                            <li>Marketing Materials</li>
+                            <li>Custom Solutions</li>
+                        </ul>
+                    </div>
+                    <div className=''>
+                        <h2 className='text-white text-xl mb-6'>Quick Links</h2>
+                        <ul className='text-gray flex flex-col gap-5 font-mont'>
+                            <a href="#" className="hover:text-gray-300">About Us</a>
+                            <a href="#" className="hover:text-gray-300">Portfolio</a>
+                            <a href="#" className="hover:text-gray-300">FAQ</a>
+                            <a href="#" className="hover:text-gray-300">Contact Us</a>
+                        </ul>
+                    </div>
+                    <div className='w-[260px]'>
+                        <h2 className='text-xl mb-6 text-white'>Contact Info</h2>
+                        <ul className='flex flex-col gap-5 font-mont'>
+                            <div className='flex items-center gap-4'>
+                                <img src={locationIcon} alt="a location icon" />
+                                <p className='leading-7'>123, George Street Shomulu, Lagos</p>
+                            </div>
+                            <div className='flex items-center gap-4'>
+                                <img src={phoneIcon} alt="a phone icon" />
+                                <p className='leading-7'>+234 123 456 78</p>
+                            </div>
+                            <div className='flex items-center gap-4'>
+                                <img src={mailIcon} alt="a mail icon" />
+                                <p className='leading-7'>info@xploreprinting.com</p>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+                <div className="mt-8 text-center text-[18px] font-light">
+                    @ {new Date().getFullYear()} Explore-Printing. All rights reserved. | Privacy Policy | Terms of Service
+                </div>
+            </div>
+        </footer>
+    </section>
+    
   )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,11 +11,104 @@ import century from '../assets/brands/century.svg';
 import gionee from '../assets/brands/gionee.svg';
 import exploreWhite from '../assets/explore-white-logo.svg';
 import exBlack from '../assets/ex-black.svg';
-import man from '../assets/MAN .svg';
+import man from '../assets/MY PIC  1.svg';
 import Footer from '../components/Footer';
 import Portfolio from '../components/Portfolio';
+import bag from '../assets/BAG (1) 1.svg';
+import michaelPic from '../assets/Micheal.svg';
+import sarahPic from '../assets/Sarah.svg';
+import davidPic from '../assets/David.svg';
+import starRating from '../assets/star-rating.svg';
+import printingProcessImg from '../assets/printing-process.svg';
+import { title } from 'process';
 
 export default function Home() {
+
+    const brandFeatures =  [
+        {
+            title: 'Brand Identity', 
+            desc: [
+                    'We create distinctive brand identities that capture your essence and communicate your story with clarity.', 'From logo design and color systems to complete visual guidelines, we ensure your brand stands out, stays consistent, and connects emotionally with your audience.'
+                ]
+        },
+        {
+            title: 'Brand Merchandise',
+            desc: [
+                'We help brands stay visible beyond the billboard. At Explore, we source high-quality products and transform them into walking brand experiences through creative printing and customization.',
+
+                'From everyday essentials to premium keepsakes, our merchandise keeps your brand in motion — because when people carry them, they carry your brand too.'
+            ]
+        },
+        {
+            title: '',
+            desc: []
+        },
+        {
+            title: 'Digital Banner',
+            desc: [
+                'We produce high-quality digital banners for retail marketing, especially for SMEs and fintech brands looking to promote their products and services through their agents.',
+
+                'We also create banners for advertising agencies, billboard campaigns, churches, and NGOs — delivering prints that command attention and drive visibility.'
+            ]
+        },
+        {
+            title: 'Rollup Stands/PopUps',
+            desc: [
+                'We produce high-quality roll-up stands and feather banners designed to elevate your marketing presence.',
+
+                'Available in a variety of sizes and styles from slim-base to flat-base designs our banners are built to suit your brand’s unique needs and make your message stand tall anywhere.'
+            ]
+        },
+        {
+            title: 'General Printing',
+            desc: [
+                'We’re your go-to destination for premium offset printing. Using our state-of-the-art Speedmaster press, we deliver exceptional quality for all your stationery and paper-based needs — from paper bags, business cards, and letterheads to business profiles, gift boxes, and more.',
+
+                'Whatever your print project, we bring precision, color accuracy, and class to every detail.'
+            ]
+        }
+
+    ]
+    const reviews = [
+        {
+            name: 'Micheal Chan',
+            img: michaelPic,
+            role: 'Marketing Director, Techstart',
+            desc: '"PrintCraft delivered exceptional quality for our trade show banners. The colors were vibrant, the material was durable, and the turnaround time was impressive. Highly recommended!"'
+        },
+        {
+            name: 'Sarah Johnson',
+            img: sarahPic,
+            role: 'Author & Publisher',
+            desc: '"The book printing quality exceeded my expectations. The binding is perfect, colors are accurate, and the customer service team was incredibly helpful throughout the process."'
+        },
+        {
+            name: 'David Rodriguez',
+            img: davidPic,
+            role: 'Restaurant Owner',
+            desc: '"From menu designs to promotional flyers, PrintCraft has been our go-to printing partner. Their attention to detail and competitive pricing make them unbeatable."'
+        }
+    ]
+
+    const printingProcess = [
+        {
+            title: 'Submit Design',
+            desc: 'Upload your artwork or work with our design team to create something amazing.'
+        },
+        {
+            title: 'Approve Proof',
+            desc: 'Review and approve the digital proof before we begin production.'
+        },
+        {
+            title: 'Printing',
+            desc: 'Our expert team prints your project using premium materials and equipment.'
+        },
+        {
+            title: 'Delivery',
+            desc: 'Fast, secure delivery to your doorstep with tracking information provided'
+        },
+        
+    ]
     const navLinks = [
         { name: 'Home', href: '/' },
         { name: 'About Us', href: '/about-us' },
@@ -81,9 +174,6 @@ export default function Home() {
             </section>
 
             <section id='about-us'>
-                <div className='mb-12'>
-                    <h1 className='text-3xl font-semibold text-center'>ABOUT US</h1>
-                </div>
                 <div className='bg-primary section-spacing text-white text-xl py-12 px-10 flex relative justify-between'>
                     <div className='flex flex-col gap-10 w-[701px]'>
                         <img src={exploreWhite} alt="explore logo" className='w-[150px]'/>
@@ -108,14 +198,115 @@ export default function Home() {
                             
                         </p>
                     </div>
-                    <div className='self-end mb-[200px] '>
+                    <div className='self-end mb-[200px] relative'>
                         <img src={exBlack} alt="explore acronym logo"/>
                     </div>
-                    <img src={man} alt="a picture of a man" className='absolute bottom-0 right-0'/>
+                    <img src={man} alt="a picture of a man" className='absolute bottom-0 right-0 '/>
+                    
                 </div>
             </section>
 
-            <section className="section-spacing mt-[100px]">
+            <section className='bg-[#FFCCCC] h-[1000px] py-16 flex jusify-center items-center flex-col gap-10 relative mb-[100px]'>
+                <h1 className='text-center text-3xl font-normal'>What we bring to the table</h1>
+                <div className='bg-white mx-25 h-full py-[90px] px-5'>
+                    <div className="grid grid-cols-3 gap-4 gap-y-10 gap-x-5">
+                        {
+                           brandFeatures.map((item, index) => (
+                            index === 2 ? (
+                                <div key={index} className="p-4 bg-transparent border-0 shadow-none">
+                                    {/* Empty space */}
+                                </div>
+                            ) : (                               
+                                <div key={index} className="p-4 border border-[#FF400A] rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300">
+                                    <h2 className="text-xl font-semibold mb-2">{item.title}</h2>
+                                    <p className="text-gray-700 text-sm mb-5">{item.desc[0]}</p>
+                                    <p className="text-gray-700 text-sm">{item.desc[1]}</p>
+                                </div>
+                           )))
+                        }
+
+                        {/* Empty space */}
+                       
+                        <div className='absolute -top-3 right-25'>
+                            <img src={bag} alt="bag" />
+                        </div>
+                    </div>
+
+
+                </div>
+            </section>
+
+
+            <section className='section-spacing flex flex-col items-center justify-center' id='clients'>
+                <div className='w-[60%]'>
+                    <h1 className='font-poppins text-4xl xl:text-5xl font-medium text-center'>What Our Clients Say</h1>
+                    <p className='text-center lg:text-lg text-sm md:text-base mt-4 font-poppins'>
+                        Don't just take our word for it - hear from satisfied customers across industries.
+                    </p>
+                </div>
+                <div className='flex gap-5'>
+                    {reviews.map((review, index) => (
+                        <div key={index} className='bg-gray-100 p-8 mt-10 rounded-lg shadow-md'>
+                            <div className='flex gap-5 mb-8'>
+                                <img src={review.img} alt={review.name} className='w-16 h-16 rounded-full mb-4'/>
+                                <div >
+                                    <h3 className='text-lg font-semibold text-gray-900'>{review.name}
+                                    </h3>
+                                    <p className='text-gray-600'>   {review.role}
+                                    </p>
+                                </div>
+                            </div>
+                            <img src={starRating} alt="star ratings image" className='mb-5'/>
+                            <p className=' text-gray-800 mb-6'>{review.desc}</p>
+                           
+                        </div>
+                    ))}
+                </div>
+
+                <div className='w-[70%] mt-13 flex justify-center gap-10'>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <h1 className='font-semibold text-4xl'>500+</h1>
+                        <p className='font-poppins text-xl text-center'>Happy Clients</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <h1 className='font-semibold text-4xl'>10k+</h1>
+                        <p className='font-poppins text-xl text-center'>Projects Completed</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <h1 className='font-semibold text-4xl'>99%</h1>
+                        <p className='font-poppins text-xl text-center'>Satisfaction Rate</p>
+                    </div>
+                    <div className='flex flex-col items-center justify-center gap-2'>
+                        <h1 className='font-semibold text-4xl'>15+</h1>
+                        <p className='font-poppins text-xl text-center'>Years of Experience</p>
+                    </div>
+                </div>
+            </section>
+
+            <section id='process' className='section-spacing'>
+                <div className='header-centered'>
+                    <h1 className='font-poppins text-4xl xl:text-5xl font-medium text-center'>Our Simple Process</h1>
+                    <p className='subtitle'>
+                        From concept to delivery, we make printing easy and hassle-free.
+                    </p>
+                </div>
+
+                <div className='mb-5 mt-18'>
+                    <img src={printingProcessImg} alt="Our printing process" />
+                </div>
+                <div className='flex justify-between gap-5'>
+                    {
+                        printingProcess.map((step, index) => (
+                            <div key={index} className='flex flex-col items-center justify-center gap-5 mb-8'>
+                                <h2 className='text-xl font-semibold'>{step.title}</h2>
+                                <p className='text-center'>{step.desc}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+            </section>
+
+            <section className="section-spacing" id='portfolio'>
                 <div className='header-centered'>
                     <h1 className='font-poppins text-4xl xl:text-5xl font-medium'>Our Work Portfolio</h1>
                     <p className='subtitle'>
