fcc610b7347ba0728166c50c8f2d8dbc178f58df
midnite-ops <charlesoyims@gmail.com>
2026-01-02 17:45:46 +0100

    Fixed the renaming issue

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -21,6 +21,7 @@ import starRating from '../assets/star-rating.svg';
 import printingProcessImg from '../assets/printing-process.svg';
 import HeroTicker from '../components/HeroTicker';
 import { SidebarTrigger } from '@/components/ui/sidebar';
+import { Accordion,AccordionContent, AccordionItem,AccordionTrigger } from "@/components/ui/accordion"
 import { MenuIcon } from 'lucide-react';
 import processImg1 from '../assets/process/img-1.svg';
 import processImg2 from '../assets/process/img-2.svg';
@@ -31,7 +32,6 @@ import faceCap from '../assets/table/face-cap.svg';
 import printer1 from '../assets/table/printer-1.svg';
 import printer2 from '../assets/table/printer-2.svg';
 import flex from '../assets/table/flex.svg';
-import Faq from '@/components/Faq';
 export default function Home() {
 
     const brandFeatures =  [
@@ -85,6 +85,8 @@ export default function Home() {
         }
 
     ]
+    const faq = ['How do I place an order with your printing service?', 'What if I don’t have a design ready?',
+    'How long does printing take?', 'How do you ensure quality and accuracy?', 'What level of print quality should I expect from your service?', 'What happens if there’s an error or issue with my order?']
     const reviews = [
         {
             name: 'Micheal Chan',
@@ -339,7 +341,33 @@ export default function Home() {
                 
             </section>
 
-            <Faq />
+            <section id="FAQ" className="flex justify-center section-spacing  space-y-20 px-2">
+
+            <Accordion
+            type="single"
+            collapsible
+            className="w-full  flex flex-col gap-7.5 justify-center "
+            defaultValue="">
+                {faq.map((item, index) => (
+                    <AccordionItem key={index} value={`item ${index}`} className='py-4 px-5 border-none bg-[#F4F4F4] rounded-xl'>
+                        <AccordionTrigger className='bg-transparent hover:no-underline'>{item}</AccordionTrigger>
+                        <AccordionContent>
+                            <div className="flex flex-col gap-4 mt-5 flex-start">
+                                <p className="text-start">
+                                    Our flagship product combines cutting-edge technology with sleek
+                                    design. Built with premium materials, it offers unparalleled
+                                    performance and reliability.
+                                </p>
+                                <p className="text-start">
+                                    Key features include advanced processing capabilities, and an
+                                    intuitive user interface designed for both beginners and experts.
+                                </p>
+                            </div>
+                        </AccordionContent>
+                    </AccordionItem>  
+                ))}
+            </Accordion>
+        </section>
         </main>
         <Footer />
     </>
