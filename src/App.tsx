42a548c3d47cce35b9972b39cbb6ba5a625121cd
midnite-ops <charlesoyims@gmail.com>
2026-04-07 21:58:32 +0100

    Restored

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from './pages/home'
+import Home from './pages/home'
 import Page404 from './pages/page404';
 import Layout from "./layout";
 import "./index.css";
@@ -10,7 +10,7 @@ function App() {
       <Layout>
         <Routes>
             <Route path="*" element={<Page404 />} /> 
-            {/* <Route path="/" element={<Home/>} /> */}
+            <Route path="/" element={<Home/>} />
             {/* <Route path="/about" element={<About />} /> */}
           </Routes>
       </Layout>
